fecb1b1bc72b3ddaa7600030564ce5f3cb5bb054
gramscott <120787031+gramscott@users.noreply.github.com>
2023-03-30 12:04:05 +0100

    Example commit

---
+++ src/main/java/Room.java
@@ -0,0 +1,19 @@
+public class Room {
+
+    private RoomType roomType;
+    public Room(RoomType roomType) {
+        this.roomType = roomType;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getValueFromEnum() {
+        return this.roomType.getValue();
+    }
+}
